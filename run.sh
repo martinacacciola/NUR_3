794227dfe566d1f0d816c0c0a0b57a65b73b066a
Martina Cacciola <127241970+martinacacciola@users.noreply.github.com>
2024-04-25 11:48:36 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,11 +10,11 @@ fi
 
 # Download files from given links if they are not already present
 echo "Downloading files..."
-#wget -nc https://home.strw.leidenuniv.nl/~daalen/Handin_files/satgals_m11.txt
-#wget -nc https://home.strw.leidenuniv.nl/~daalen/Handin_files/satgals_m12.txt
-#wget -nc https://home.strw.leidenuniv.nl/~daalen/Handin_files/satgals_m13.txt
-#wget -nc https://home.strw.leidenuniv.nl/~daalen/Handin_files/satgals_m14.txt
-#wget -nc https://home.strw.leidenuniv.nl/~daalen/Handin_files/satgals_m15.txt
+wget -nc https://home.strw.leidenuniv.nl/~daalen/Handin_files/satgals_m11.txt
+wget -nc https://home.strw.leidenuniv.nl/~daalen/Handin_files/satgals_m12.txt
+wget -nc https://home.strw.leidenuniv.nl/~daalen/Handin_files/satgals_m13.txt
+wget -nc https://home.strw.leidenuniv.nl/~daalen/Handin_files/satgals_m14.txt
+wget -nc https://home.strw.leidenuniv.nl/~daalen/Handin_files/satgals_m15.txt
 
 # Run satellite.py script
 echo "Run the Satellite script ..."
@@ -22,10 +22,10 @@ python3 satellite.py
 
 echo "Generating the PDF"
 
-#pdflatex hand-in-3.tex
-#bibtex hand-in-3.aux
-#pdflatex hand-in-3.tex
-#pdflatex hand-in-3.tex
+pdflatex hand-in-3.tex
+bibtex hand-in-3.aux
+pdflatex hand-in-3.tex
+pdflatex hand-in-3.tex
 
 
 echo "Script execution completed."
